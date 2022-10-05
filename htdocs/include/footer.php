4a3159b909b9aaa67191c1fc9b1746d26dc9623f
KwonHyehyeon <109262001+KwonHyehyeon@users.noreply.github.com>
2022-10-05 10:09:24 +0900

    20221005

---
+++ htdocs/include/footer.php
@@ -0,0 +1,8 @@
+<footer id="footer">
+    <h2>푸터 영역입니다.</h2>
+    <div class="footer__inner container">
+        <address>Copyright @2022 noeyheyh</address>
+        <div>blog by noeyheyh</div>
+    </div>
+</footer>
+    <!-- //footer -->
